602ef087762417d96b14e081b828a549f13d41bd
Sage Berg <sberg@uoregon.edu>
2016-07-12 18:04:17 -0700

    Clean up code

--- client.js
+++ client.js
@@ -60,27 +60,15 @@ function bindKeys() {
 }
 
 function move(rowInc, colInc) {
-  /*
-  var blockRow = CURRENT_BLOCK.row + Math.floor(CHUNK_SIZE / 2) + rowInc;
-  var blockCol = CURRENT_BLOCK.col + Math.floor(CHUNK_SIZE / 2) + colInc;
-  var nextBlock =
-    CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col][blockRow][blockCol];
-  console.log(nextBlock);
-  */
-  if (chunkBoundsCheck(CURRENT_CHUNK.row + rowInc,
-    CURRENT_CHUNK.col + colInc)){// && nextBlock !== 0) {
+  var chunkRow = CURRENT_CHUNK.row;
+  var chunkCol = CURRENT_CHUNK.col;
+  if (chunkBoundsCheck(chunkRow + rowInc, chunkCol + colInc)) {
     changeCurrentBlock(rowInc, colInc);
-
-    if (CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col] === null) {
-      loadInfluencedChunk(CURRENT_CHUNK.row, CURRENT_CHUNK.col);
-      setTimeout(function() {
-        console.log("slow because of loading chunks");
-        waitForStichPrep();
-      }, 700);
+    if (CHUNKS[chunkRow][chunkCol] === null) {
+      loadInfluencedChunk(chunkRow, chunkCol, stitchChunksPrep);
     } else {
-        waitForStichPrep();
+      stitchChunksPrep();
     }
-
   } else {
     console.log("move not permitted");
   } //end bounds check if
@@ -97,26 +85,12 @@ function changeCurrentBlock(row, col) {
   changeCurrentChunk("col");
 }
 
-function loadInfluencedChunk(row, col) {
-  if (CHUNKS[row][col] === null) {
-    var presetPotentialTiles = getPresetPotentialTiles(row, col);
-    $.post("influenced_map.json", presetPotentialTiles, function(res) {
-      CHUNKS[row][col] = res;
-    });
-  }
-}
-
-function waitForStichPrep() {
-  var grid = stitchChunksPrep(buildBlankGrid())
-  if (CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col] === null ||
-      CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col + 1] === null ||
-      CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col + 1] === null) {
-    setTimeout(function() {
-      $("#map").html(genMapHTML(grid));
-    }, 1000);
-  } else {
-    $("#map").html(genMapHTML(grid));
-  }
+function loadInfluencedChunk(row, col, callback) {
+  var presetPotentialTiles = getPresetPotentialTiles(row, col);
+  $.post("influenced_map.json", presetPotentialTiles, function(res) {
+    CHUNKS[row][col] = res;
+    callback();
+  });
 }
 
 function changeCurrentChunk(rowOrCol) {
@@ -216,29 +190,46 @@ function genMapHTML(grid) {
   return mapHTML;
 }
 
-function stitchChunksPrep(grid) {
-  if (CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col] === null ||
-      CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col + 1] === null ||
-      CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col + 1] === null) {
-    loadInfluencedChunk(CURRENT_CHUNK.row + 1, CURRENT_CHUNK.col);
-    loadInfluencedChunk(CURRENT_CHUNK.row, CURRENT_CHUNK.col + 1);
-    loadInfluencedChunk(CURRENT_CHUNK.row + 1, CURRENT_CHUNK.col + 1);
-    setTimeout(function() {
-      stitchChunks(grid);
-    }, 1000);
-  } else {
-    stitchChunks(grid);
+function stitchChunksPrep() {
+  if (CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col] === null) {
+    loadDownGrid();
   }
-  return grid;
+  if (CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col + 1] === null) {
+    loadRightGrid();
+  }
+  if (CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col + 1] === null) {
+    loadDownRightGrid();
+  }
+  stitchChunks();
 }
 
-function stitchChunks(grid) {
+function loadDownGrid() {
+  loadInfluencedChunk(CURRENT_CHUNK.row + 1, CURRENT_CHUNK.col, stitchChunks);
+}
+
+function loadRightGrid() {
+  loadInfluencedChunk(CURRENT_CHUNK.row, CURRENT_CHUNK.col + 1, stitchChunks);
+}
+
+function loadDownRightGrid() {
+  loadInfluencedChunk(CURRENT_CHUNK.row + 1, CURRENT_CHUNK.col + 1, stitchChunks);
+}
+
+function stitchChunks() {
+  var grid = buildBlankGrid();
   var row = CURRENT_BLOCK.row;
   var col = CURRENT_BLOCK.col;
   var currentGrid = CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col];
   var downGrid = CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col];
   var rightGrid = CHUNKS[CURRENT_CHUNK.row][CURRENT_CHUNK.col + 1];
   var downRightGrid = CHUNKS[CURRENT_CHUNK.row + 1][CURRENT_CHUNK.col + 1];
+
+  if (currentGrid === null || downGrid === null || rightGrid === null ||
+      downRightGrid === null) {
+        console.log("too early");
+        return;
+  }
+
   for (var i = 0; i < CHUNK_SIZE; i++) {
     for (var j = 0; j < CHUNK_SIZE; j++) {
       var adjustedRow = i + row;
@@ -255,7 +246,8 @@ function stitchChunks(grid) {
       }
     } //end for i loop
   } //end for j loop
-  return grid;
+  $("#map").html(genMapHTML(grid));
+  //return grid;
 }
 
 function buildBlankGrid() {
