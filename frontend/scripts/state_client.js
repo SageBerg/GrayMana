8cfeb45eb16d00e85349d9d1e1287ac9a5c21564
Sage Berg <sberg@uoregon.edu>
2016-07-29 15:35:26 -0700

    Organize front-end files

--- frontend/scripts/state_client.js
+++ frontend/scripts/state_client.js
@@ -6,16 +6,19 @@ var HUDState = {
 var world = {
   chunkSize: 40,
   chunks: {},
+
+  //represent the player's view, not necessarily where their character is
   currentBlock: {row: 0, col: 0},
   currentChunk: {x: 0, y: 0}
+
 };
 
 var character = {
   damage: {red: 0, orange: 0, yellow: 0, green: 0, blue: 0, indigo: 0,
     violet: 0, black: 0, gray: 0, white: 0},
   life: 100,
-  mana: {red: 0, orange: 0, yellow: 0, green: 0, blue: 0, indigo: 0, violet: 0,
-    black: 0, gray: 0, white: 0},
+  mana: {red: 0, orange: 0, yellow: 0, green: 0, blue: 0, indigo: 0,
+    violet: 0, black: 0, gray: 0, white: 0},
   runes: {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0,
     l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0,
     x: 0, y: 0, z: 0, ' ': 0},
@@ -28,7 +31,8 @@ function getChunkCoords() {
 }
 
 function getIncrementedChunkCoords(xInc, yInc) {
-  return (world.currentChunk.x + xInc) + ' ' + (world.currentChunk.y + yInc);
+  return (world.currentChunk.x + xInc) + ' ' +
+    (world.currentChunk.y + yInc);
 }
 
 function changeCurrentBlock(row, col) {
@@ -101,8 +105,8 @@ function deathCheck() {
 }
 
 function clearDamage() {
-  character.mana = {red: 0, orange: 0, yellow: 0, green: 0, blue: 0, indigo: 0,
-    violet: 0, black: 0, gray: 0, white: 0};
+  character.mana = {red: 0, orange: 0, yellow: 0, green: 0, blue: 0,
+    indigo: 0, violet: 0, black: 0, gray: 0, white: 0};
 }
 
 function printDeathMessage(color) {
