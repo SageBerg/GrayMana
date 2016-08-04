e6c7e9707b344279ae824c99470fd6a8c99035e9
Sage Berg <sberg@uoregon.edu>
2016-08-04 16:28:34 -0700

    Have damage view update when you take damage

--- frontend/angular_gray_mana_controller.js
+++ frontend/angular_gray_mana_controller.js
@@ -1,6 +1,4 @@
-app.controller("grayManaController", function($scope) {
-  $scope.colorClasses = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
-    'violet', 'black', 'gray', 'white'];
+app.controller('grayManaController', function($scope) {
   $scope.damageTypeIds = ['damage-1-red', 'damage-2-orange', 'damage-3-yellow',
     'damage-4-green', 'damage-5-blue', 'damage-6-indigo', 'damage-7-violet',
     'damage-8-black', 'damage-9-gray', 'damage-10-white'];
@@ -19,5 +17,30 @@ app.controller("grayManaController", function($scope) {
       return '';
     }
   }
+
+  $scope.colorClasses = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
+    'violet', 'black', 'gray', 'white'];
+  $scope.damages = {red: 0, orange: 0, yellow: 0, green: 0, blue: 0, indigo: 0,
+    violet: 0, black: 0, gray: 0, white: 0
+  };
+
+  $scope.widthObj = {
+    red: {'width': $scope.damages['red'] + '%'},
+    orange: {'width': $scope.damages['orange'] + '%'},
+    yellow: {'width': $scope.damages['yellow'] + '%'},
+    green: {'width': $scope.damages['green'] + '%'},
+    blue: {'width': $scope.damages['blue'] + '%'},
+    indigo: {'width': $scope.damages['indigo'] + '%'},
+    violet: {'width': $scope.damages['violet'] + '%'},
+    black: {'width': $scope.damages['black'] + '%'},
+    gray: {'width': $scope.damages['gray'] + '%'},
+    white: {'width': $scope.damages['white'] + '%'}
+  };
+
+  $scope.takeSomeDamage = function(amount, color) {
+    $scope.damages[color] += amount;
+    $scope.widthObj[color].width = parseInt(($scope.widthObj[color].width).slice(0, -1)) + amount + '%';
+    console.log($scope.widthObj[color]);
+  };
 
 });
