702085f883584a15177e9a6174b7fee80b25f054
Sage Berg <sberg@uoregon.edu>
2016-08-05 15:07:01 -0700

    Use Angular to render 'Learn Spells' list

--- frontend/angular_gray_mana_controller.js
+++ frontend/angular_gray_mana_controller.js
@@ -49,7 +49,7 @@ app.controller('grayManaController', function($scope) {
     $scope.showModal = true;
     switch (modalContent) {
       case 'learnSpells':
-        $scope.initLearnSpellsModal();
+        $scope.showLearnSpells = true;
         break;
       case 'editOptions':
         $scope.showEditOptions = true;
@@ -64,11 +64,6 @@ app.controller('grayManaController', function($scope) {
 
   $scope.showEditOptions = false;
   $scope.showLearnSpells = false;
-
-  $scope.initLearnSpellsModal = function() {
-    $scope.showLearnSpells = true;
-    renderLearnSpellList();
-  }
 
   $scope.closeModal = function() {
     $scope.showModal = false;
@@ -100,12 +95,54 @@ app.controller('grayManaController', function($scope) {
     } else {
       return 'rune-faded';
     }
-  }
+  };
   $scope.letterOrQuotes = function(letter) {
     if (letter === ' ') {
       return '\' \''
     }
     return letter;
+  };
+
+  $scope.spellList = SPELL_LIST;
+  $scope.knownSpells = new Set();
+  $scope.runeCost = function(spell) {
+    return spell.length;
+  };
+
+  $scope.isLearnable = function(spellName) {
+    var spellName = spellName.toLowerCase();
+    var runesCopy = $.extend({}, $scope.runes);
+    if ($scope.knownSpells.has(spellName)) {
+      return false;
+    }
+    for (var i = 0; i < spellName.length; i++) {
+      if (runesCopy[spellName[i]] === 0) {
+        return false;
+      } else {
+        runesCopy[spellName[i]] -= 1;
+      }
+    }
+    return true;
   }
+
+  $scope.getLearnableSpells = function() {
+    var learnableSpells = [];
+    for (var spell of $scope.spellList) {
+      if ($scope.isLearnable(spell)) {
+        learnableSpells.push(spell);
+      }
+    }
+    return learnableSpells;
+  };
+
+  $scope.getUnlearnableSpells = function() {
+    var unlearnableSpells = [];
+    for (var spell of $scope.spellList) {
+      if (!$scope.isLearnable(spell)) {
+        unlearnableSpells.push(spell);
+      }
+    }
+    return unlearnableSpells;
+  };
 
 });
