d76ea5f624630249f6b259ad0c3c11e214ae2af5
Sage Berg <sberg@uoregon.edu>
2016-08-16 16:32:31 -0700

    Bind hotkeys with Angular events

---
+++ frontend/services/hotkeys.js
@@ -0,0 +1,19 @@
+app.service('hotKeys', function() {
+  this.manatateHotKey = 'm';
+  this.cloneHotKey = 'c';
+
+  this.interactHotKey = 'k';
+  this.eatHotKey = 'e';
+  this.sleepHotKey = 'z';
+
+  this.learnSpellsHotKey = 'l';
+  this.prepareSpellsHotKey = 'p';
+  this.castSelectedSpellHotKey = 'r';
+
+  this.talkHotKey = 't';
+  this.questHotKey = 'q';
+  this.factionHotKey = 'f';
+
+  this.inventoryHotKey = 'i';
+  this.achievementsHotKey = 'v';
+});
