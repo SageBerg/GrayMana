fd2937752bdf888125feb54dc56f1b6fc2b01c22
Sage Berg <sberg@uoregon.edu>
2016-08-16 12:26:51 -0700

    Start adding and improving object organization

---
+++ objects/account.js
@@ -0,0 +1,7 @@
+var Account = function() {
+  this.id = 0;
+  this.characters = [];
+  this.email = 'person@example.com';
+  this.password = 'hashed_password';
+  this.suspended = false;
+};
---
+++ objects/faction.js
@@ -0,0 +1,7 @@
+var Faction = function() {
+  this.factionName = 'The Order of the South Coast';
+  this.members = ['characterOne', 'characterTwo'];
+  this.government = 'governmentObject';
+  this.laws = [];
+  this.territory = [];
+};
