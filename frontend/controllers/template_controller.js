1bd28e10bd78ebeb026a20a3641d5c3a009584e6
Sage Berg <sberg@uoregon.edu>
2016-09-22 22:12:05 -0700

    Make client log out users who close the tab or navigate away from it

--- frontend/controllers/template_controller.js
+++ frontend/controllers/template_controller.js
@@ -1,5 +1,13 @@
-app.controller('templateController', function($scope, $http) {
+app.controller('templateController', function($scope, $http, $window) {
   $scope.currentTemplate = 'login.html';
+
+  $window.onbeforeunload = function() {
+    return $http({
+      method: 'POST',
+      url: 'logout',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }) //.then(function(res){});
+  };
 
   $scope.$on('changeToCharacterTemplate', function() {
     $scope.currentTemplate = 'character.html';
